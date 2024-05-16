331521bc81f1ad3e441271d236b5a4214754b67f
JesusDavid2002 <jesusdavidsepu@gmail.com>
2024-05-16 22:33:21 +0200

    reajuste de html a php, creación de cards de categorias y productos, reajuste del home

---
+++ php/footer.php
@@ -0,0 +1,16 @@
+<footer class="mt-3 ps-5">
+    <ul class="nav flex-column flex-md-row">
+            <h4 class="nav-link active" aria-current="page" href="#">Dirección</h4>
+        
+            <h4 class="nav-link" href="#">Teléfono</h4>
+        
+            <h4 class="nav-link" href="#">Políticas de Privacidad</h4>
+        
+            <h4 class="nav-link"  href="#">Políticas de Devolución</h4>
+        
+        </ul>
+</footer>
+
+<script src="http://localhost/jdsc-ucam-pw-practicafinal/js/logo.js"></script>
+<script src="http://localhost/jdsc-ucam-pw-practicafinal/js/maps.js"></script>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAMwjj_6pBh-9HRSxBOJV1RjeZUNbduJpo&callback=iniciarMap"></script>
